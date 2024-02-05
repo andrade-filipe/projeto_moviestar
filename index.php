bcde8a075adf888ef5ee8c4c7a1393d941e7f65d
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-05 16:44:12 -0300

    variável de base url criada

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once("globals.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,9 +17,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+
     <h1>Testando MovieStar</h1>
     <i class="far fa-plus-square"></i>
-</body>
-<!-- BOOTSTRAP JS -->
+
+    <!-- BOOTSTRAP JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
 </html>
