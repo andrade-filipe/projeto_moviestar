b97e8003a0ded1bd04659b3768fabf353139988a
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-09 14:04:29 -0300

    criação do DAO

---
+++ dao/UserDAO.php
@@ -0,0 +1,63 @@
+<?php
+    include_once("models/User.php");
+
+    class UserDAO implements UserDAOInterface{
+        private $connection;
+        private $url;
+
+        public function __construct(PDO $connection, $url){
+            $this -> connection = $connection;
+            $this -> url = $url;
+        }
+
+        public function buildUser($data){
+            $user = new User();
+
+            $user -> id = $data["id"];
+            $user -> name = $data["name"];
+            $user -> lastname = $data["lastname"];
+            $user -> password = $data["password"];
+            $user -> image = $data["image"];
+            $user -> bio = $data["bio"];
+            $user -> token = $data["token"];
+
+            return $user;
+        }
+
+        public function create(User $user, $authUser = false){
+
+        }
+
+        public function update(User $user){
+
+        }
+
+        public function verifyToken($protected = false){
+
+        }
+
+        public function setTokenToSession($token, $redirect = true){
+
+        }
+
+        public function authenticateUser($email, $password){
+
+        }
+
+        public function findByEmail($email){
+
+        }
+
+        public function findById($id){
+
+        }
+
+        public function findByToken($token){
+
+        }
+
+        public function changePassword(User $user){
+
+        }
+    }
+?>
