32b73fffee312646f8310ea6e60f0a2efea7402f
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-09 11:42:21 -0300

    separado os templates

---
+++ templates/footer.php
@@ -0,0 +1,35 @@
+<footer id="footer">
+        <div id="social-container">
+            <ul id="footer-links-container">
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook-square"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li>
+                    <a href="#">Adicionar Filme</a>
+                    <a href="#">Adicionar Crítica</a>
+                    <a href="#">Entrar / Registrar</a>
+                </li>
+            </ul>
+        </div>
+        <p>&copy; 2020 Hora de Codar. All Rights Reserved</p>
+    </footer>
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
