d32ef820b2e83a4489e2b76f1a4f1ceac983270c
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-13 22:14:53 -0300

    busca por categoria

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -49,7 +49,23 @@
         }
 
         public function getMoviesByCategory($category){
+            $movies = [];
 
+            $stmt = $this -> connection -> prepare("SELECT * FROM movies WHERE category = :category ORDER BY id DESC ");
+
+            $stmt -> bindParam(":category", $category);
+
+            $stmt -> execute();
+
+            if($stmt -> rowCount() > 0){
+                $moviesArray = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+                foreach($moviesArray as $movie){
+                    $movies[] = $this -> buildMovie($movie);
+                }
+            }
+
+            return $movies;
         }
 
         public function getMoviesByUserId($id){
