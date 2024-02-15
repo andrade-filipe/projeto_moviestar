4b3bc7a359cef4d08b4f45a254c41e8fd4734df2
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-15 01:16:53 -0300

    funcionalidade: criar reviews

--- dao/ReviewDAO.php
+++ dao/ReviewDAO.php
@@ -2,6 +2,7 @@
 
     include_once("models/Review.php");
     include_once("models/Message.php");
+    include_once("dao/UserDAO.php");
     class ReviewDAO implements ReviewDAOInterface{
         private $connection;
         private $message;
@@ -12,10 +13,34 @@
         }
 
         public function buildReview($data){
+            $reviewObject = new Review();
 
+            $reviewObject -> id = $data["id"];
+            $reviewObject -> rating = $data["rating"];
+            $reviewObject -> review = $data["review"];
+            $reviewObject -> users_id = $data["users_id"];
+            $reviewObject -> movies_id = $data["movies_id"];
+
+            return $reviewObject;
         }
         public function create(Review $review){
+            $this->connection->beginTransaction();
+            try{
+                $stmt = $this->connection->prepare(
+                "INSERT INTO reviews (rating, review, users_id, movies_id) "
+                . "VALUES (:rating, :review, :users_id, :movies_id)"
+                );
 
+                $stmt -> bindParam(":rating", $review->rating);
+                $stmt -> bindParam(":review", $review->review);
+                $stmt -> bindParam(":users_id", $review->users_id);
+                $stmt -> bindParam(":movies_id", $review->movies_id);
+
+                $stmt -> execute();
+            }catch(PDOException $e){
+                $this->connection->rollBack();
+            }
+            $this->connection->commit();
         }
         public function getMoviesReview($id){
 
--- review_process.php
+++ review_process.php
@@ -3,11 +3,43 @@
     include_once("db.php");
     include_once("models/Movie.php");
     include_once("models/Message.php");
+    include_once("models/Review.php");
     include_once("dao/UserDAO.php");
     include_once("dao/MovieDAO.php");
+    include_once("dao/ReviewDAO.php");
+
+    $message = new Message();
+    $userDao = new UserDAO($db_connection);
+    $movieDao = new MovieDAO($db_connection);
+    $reviewDao = new ReviewDAO($db_connection);
 
     $type = filter_input(INPUT_POST, "type");
 
-    if($type === "create"){
+    $userData = $userDao -> verifyToken();
 
+    if($type === "create"){
+        $rating = filter_input(INPUT_POST,"rating");
+        $review = filter_input(INPUT_POST,"review");
+        $movies_id = filter_input(INPUT_POST,"movies_id");
+        $users_id = $userData -> id;
+
+        $reviewObject = new Review();
+
+        $movieData = $movieDao -> findById($movies_id);
+
+        if($movieData){
+            if(!empty($rating) && !empty($review) && !empty($movies_id)){
+                $reviewObject -> rating = $rating;
+                $reviewObject -> review = $review;
+                $reviewObject -> movies_id = $movies_id;
+                $reviewObject -> users_id = $users_id;
+
+                $reviewDao -> create($reviewObject);
+                $message -> setMessage("Crítica criada", "success");
+            } else {
+                $message -> setMessage("Insira Nota e Comentário", "error");
+            }
+        }
+    } else {
+        $message -> setMessage("Informações Inválidas", "error");
     }
